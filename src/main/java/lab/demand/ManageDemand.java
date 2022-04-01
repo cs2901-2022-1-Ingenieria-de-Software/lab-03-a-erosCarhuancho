9f8e9ee128feb9e573953f54266cfc53c7418b11
erosCarhuancho <eros.carhuancho@utec.edu.pe>
2022-03-31 21:17:27 -0500

    prueba1

--- src/main/java/lab/demand/ManageDemand.java
+++ src/main/java/lab/demand/ManageDemand.java
@@ -12,26 +12,26 @@ public class ManageDemand {
 
     public double calculateTotal(List<Order> orders){
         // Calculate Taxes
-        double taxes = 0.0;
-        for (Order order : orders) {
-            double tax = this.tax.calculateTax(order.getCountry());
-            taxes += tax;
-        }
-
         // Calculate Total
+        double taxes = 0.0;
         double quantities = 0.0;
         for (Order order : orders) {
-            double temp = order.getQuantity();
-            quantities += temp;
+            taxes += this.tax.calculateTax(order.getCountry());
+            quantities += order.getQuantity();
         }
-
         return quantities * taxes;
     }
 
     public double calculateTotalForWithAdditionalByCountry(List<Order> orders, double defaultAdditionalColombia, double defaultAdditionalPeru, double defaultAdditionalBrazil){
         // Calculate additionals by country
         double taxes = 0.0;
+        double quantities = 0.0;
         for (Order order : orders) {
+            quantities +=order.getQuantity();
+
+
+            //taxes += this.tax.calculateTax(order.getCountry());
+
             String currCountry = order.getCountry();
             if (currCountry.equals("PE")) {
                 taxes += defaultAdditionalPeru;
@@ -40,15 +40,8 @@ public class ManageDemand {
             } else {
                 taxes += defaultAdditionalColombia;
             }
+             
         }
-
-        // Calculate Total
-        double quantities = 0.0;
-        for (Order order : orders) {
-            double temp = order.getQuantity();
-            quantities += temp;
-        }
-
         return quantities * taxes;
     }
 
